fded3ed7cbc2caab23705637556b1130e957a6ce
noeldrums17 <67615585+NoelDevs17@users.noreply.github.com>
2022-12-13 15:38:16 -0400

    ultimos cambios

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styleUrls: ['./por-pais.component.css']
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent{
 
-  constructor() { }
+  termino: string = '';
+  Error: boolean= false;
+  constructor(private paisService: PaisService) { }
 
-  ngOnInit(): void {
+  buscar(){
+    this.Error = false
+    return this.paisService.buscarPais(this.termino).subscribe(
+      (resp) => {
+        console.log(resp);
+      }, (err) => {
+     this.Error = true;
+      });
   }
 
 }
